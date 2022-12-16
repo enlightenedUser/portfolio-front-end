59242a5f56a3110f9be8cc0e142cb3fcd81ed386
Pavel Bulaienko <p.bulaienko@ioalabs.com>
2022-12-16 23:23:36 +0200

    feat: add projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,8 @@
+import { v4 as uuid } from "uuid";
+
 import "./projects.scss";
 import projectsList from "../../data/projects-list.json";
+import arrowSvg from "../../assets/images/arrow.svg";
 
 const Projects = () => {
   return (
@@ -8,10 +11,22 @@ const Projects = () => {
         Pr<i>o</i>jects
       </h2>
       {projectsList.map((item) => (
-        <div className="flex project-item">
-          <p>{item.title}</p>
-          <p>{item.projectType}</p>
-          <p>{item.projectTechnology}</p>
+        <div
+          className="flex project-item justify-between align-center"
+          key={uuid()}
+        >
+          <a href={item.url} target="_blank" rel="noreferrer nofollow">
+            <p className="p30">{item.title}</p>
+          </a>
+          <div className="flex description-wrapper justify-between align-center">
+            <div className="flex align-center">
+              <p className="project-type p15 main-color">{item.projectType}</p>
+              <p className="p15 main-color">{item.projectTechnology}</p>
+            </div>
+            <a href={item.url} target="_blank" rel="noreferrer nofollow">
+              <img src={arrowSvg} alt="arrow" />
+            </a>
+          </div>
         </div>
       ))}
     </div>
