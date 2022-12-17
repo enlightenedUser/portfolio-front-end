8717747853d5aad9801eb69634a69b4e7455a7a8
Pavel Bulaienko <p.bulaienko@ioalabs.com>
2022-12-17 11:03:01 +0200

    feat: add Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,28 +7,32 @@ import arrowSvg from "../../assets/images/arrow.svg";
 const Projects = () => {
   return (
     <div className="container">
-      <h2>
-        Pr<i>o</i>jects
-      </h2>
-      {projectsList.map((item) => (
-        <div
-          className="flex project-item justify-between align-center"
-          key={uuid()}
-        >
-          <a href={item.url} target="_blank" rel="noreferrer nofollow">
-            <p className="p30">{item.title}</p>
-          </a>
-          <div className="flex description-wrapper justify-between align-center">
-            <div className="flex align-center">
-              <p className="project-type p15 main-color">{item.projectType}</p>
-              <p className="p15 main-color">{item.projectTechnology}</p>
-            </div>
+      <div className="projects-wrapper" id="projects-section">
+        <h2 className="title">
+          Pr<i>o</i>jects
+        </h2>
+        {projectsList.map((item) => (
+          <div
+            className="flex project-item justify-between align-center"
+            key={uuid()}
+          >
             <a href={item.url} target="_blank" rel="noreferrer nofollow">
-              <img src={arrowSvg} alt="arrow" />
+              <p className="p30">{item.title}</p>
             </a>
+            <div className="flex description-wrapper justify-between align-center">
+              <div className="flex align-center">
+                <p className="project-type p15 main-color">
+                  {item.projectType}
+                </p>
+                <p className="p15 main-color">{item.projectTechnology}</p>
+              </div>
+              <a href={item.url} target="_blank" rel="noreferrer nofollow">
+                <img src={arrowSvg} alt="arrow" />
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
