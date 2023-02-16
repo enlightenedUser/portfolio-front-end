898275cf8395b5fb7f6e33f8cd1a325c2862b9aa
Pavel Bulaienko <p.bulaienko@ioalabs.com>
2023-02-16 14:03:06 +0200

    changes

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,4 +1,5 @@
 import "./experience.scss";
+import workList from "../../data/work-list.json";
 
 const Experience = () => {
   return (
@@ -7,42 +8,25 @@ const Experience = () => {
         <h2 className="black title">
           Ex<i>p</i>erience
         </h2>
-        <div className="work-item flex justify-between">
-          <p className="p30">Front-end Developer</p>
-          <div className="flex description-wrapper justify-between">
-            <div>
-              <p className="p15">Company: </p>
-              <p className="p15">Technology: </p>
-              <br />
-              <p className="p15">Terms: </p>
-            </div>
-            <div className="company-description">
-              <p className="p15 white-main">Nimbus</p>
-              <p className="p15 white-main">DAO-governed platform</p>
-              <p className="p15 white-main">React /HTML / CSS /JavaScript</p>
-              <p className="p15 white-main">
-                September 2021 - December 2022 (1 year 4 month)
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="work-item flex justify-between">
-          <p className="p30">Front-end Developer</p>
-          <div className="flex description-wrapper justify-between">
-            <div>
-              <p className="p15">Company: </p>
-              <p className="p15">Technology: </p>
-              <br />
-              <p className="p15">Terms: </p>
-            </div>
-            <div className="company-description">
-              <p className="p15 white-main">Terrapromo</p>
-              <p className="p15 white-main">Website development</p>
-              <p className="p15 white-main">HTML / CSS /PHP /JavaScript</p>
-              <p className="p15 white-main">March 2021 - June 2021 (4 month)</p>
+        {workList.map((work, index) => (
+          <div key={index} className="work-item flex justify-between">
+            <p className="p30">{work.title}</p>
+            <div className="flex description-wrapper justify-between">
+              <div>
+                <p className="p15">Company: </p>
+                <p className="p15">Technology: </p>
+                <br />
+                <p className="p15">Terms: </p>
+              </div>
+              <div className="company-description">
+                <p className="p15 white-main">{work.company}</p>
+                <p className="p15 white-main">{work.description}</p>
+                <p className="p15 white-main">{work.technology}</p>
+                <p className="p15 white-main">{work.terms}</p>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,58 +1,33 @@
 import "./skills.scss";
+import skillList from "../../data/skills.json";
 
 const Skills = () => {
   return (
     <>
       <div className="container">
-        <div className="skills-section">
+        <div className="skills-section title">
           <h2 className="h2 title">
             Skill<i>s</i>
           </h2>
+        </div>
+      </div>
+
+      {skillList.map((skill, index) => (
+        <div key={index} className={`skills-section ${skill.bgColor}`}>
           <div className="skills-item">
             <div className="skills-list">
-              <p className="p30 list-title">Tech</p>
-              <div className="flex">
-                <p className="skills-list-item">
-                  React (router, redux, hooks, rest API)
-                </p>
-                <p className="skills-list-item">HTML</p>
-              </div>
-              <div className="flex">
-                <p className="skills-list-item">JavaScript</p>
-                <p className="skills-list-item">Webpack</p>
-                <p className="skills-list-item">TypeScript</p>
-                <p className="skills-list-item">CSS</p>
-              </div>
-              <div className="flex">
-                <p className="skills-list-item">Web3.js, Ethers.js</p>
-                <p className="skills-list-item">GIT</p>
-                <p className="skills-list-item">
-                  Adaptive and responsive coding
-                </p>
+              <p className="p30 list-title">{skill.type}</p>
+              <div className="flex wrap">
+                {skill.list.map((skillItem, index) => (
+                  <p key={index} className="skills-list-item">
+                    {skillItem}
+                  </p>
+                ))}
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="skills-section white">
-        <div className="skills-item">
-          <div className="skills-list">
-            <p className="p30 list-title">Soft</p>
-            <div className="flex">
-              <p className="skills-list-item">Scrum</p>
-              <p className="skills-list-item">Teamwork</p>
-            </div>
-            <div className="flex">
-              <p className="skills-list-item">Agile</p>
-              <p className="skills-list-item">Perseverance</p>
-            </div>
-            <div className="flex">
-              <p className="skills-list-item">GTD</p>
-              <p className="skills-list-item">Desire to develop</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
       <div className="marginner"></div>
     </>
   );
